77a44e82da9ce446b32677040e246a2cbc538769
maya17power <tavo1.ortega1@gmail.com>
2017-08-29 12:37:22 -0700

    Adding false bouncer to repo.

---
+++ False_Bouncer/README.md
@@ -0,0 +1,12 @@
+Remove all falsy values from an array.
+
+Falsy values in JavaScript are:
+
+false, null, 0, "", undefined, and NaN.
+
+Remember to use Read-Search-Ask if you get stuck. Write your own code.
+
+Here are some helpful links:
+
+Boolean Objects
+Array.prototype.filter()
---
+++ False_Bouncer/false_bouncer.js
@@ -0,0 +1,32 @@
+/*
+Remove all falsy values from an array.
+
+Falsy values in JavaScript are:
+
+false, null, 0, "", undefined, and NaN.
+
+Remember to use Read-Search-Ask if you get stuck. Write your own code.
+
+Here are some helpful links:
+
+Boolean Objects
+Array.prototype.filter()
+*/
+
+function bouncer(arr) {
+return arr.filter(Boolean);
+}
+
+bouncer([7, "ate", "", false, 9]);
+
+//bouncer([7, "ate", "", false, 9]);
+//bouncer(["a", "b", "c"]);
+//bouncer([false, null, 0, NaN, undefined, ""]);
+//bouncer([1, null, NaN, 2, undefined]);
+
+/*
+bouncer([7, "ate", "", false, 9]) should return [7, "ate", 9].
+bouncer(["a", "b", "c"]) should return ["a", "b", "c"].
+bouncer([false, null, 0, NaN, undefined, ""]) should return [].
+bouncer([1, null, NaN, 2, undefined]) should return [1, 2].
+*/
