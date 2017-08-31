74d27ecf5aba8cb258f24c800c497798caa89332
maya17power <tavo1.ortega1@gmail.com>
2017-08-30 20:21:57 -0700

    Adding Ternary to repo.

---
+++ Ternary/README.md
@@ -0,0 +1,3 @@
+//Ternary testing
+//If num is greater 3 ,Return the number minus 3;
+//Other wise : return the number
---
+++ Ternary/ternary.js
@@ -0,0 +1,8 @@
+function strRange(str, num){
+//Ternary testing
+//If num is greater 3 ,Return the number minus 3;
+//Other wise : return the number
+  return num > 3 ? num - 3 : num;
+}
+
+strRange("my life", 8);
